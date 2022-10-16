97ba5840c421181dcf8b62cff93265bb3ff0cf3d
David Mendes <david.mendes@mindswap.academy>
2022-10-16 21:02:15 +0100

    wip all at once

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
 const prompts = require('prompts');
+//import {newsArticle} from './model.js'
 
-const thisUrl = () => fetch("https://hn.algolia.com/api/v1/search")
-    .then((response) => {
-        response.json
-        .then((news) => {news.json.json})
-        }).then(console.log);
+class newsArticle {
+    title;
+    src;
+    url;
+    author;
+    date;
+    constructor(title, src, url, author, date) {
+      this.title = title;
+      this.src = src;
+      this.url = url;
+      this.author = author;
+      this.date = date;
+    }
+};
 
-function print(){
-    return console.log(thisUrl)
+// função getFirstArticle que retorna uma Promise para os detalhes da primeira notícia
+const apiUrl = "https://hn.algolia.com/api/v1/search";
+function getAlgolia(url){
+    return fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+            let news = [];
+            json.hits.forEach(e => {
+                news.push(new newsArticle(
+                    e.title, 
+                    'algolia',
+                    e.url, 
+                    e.author, 
+                    e.created_at
+                 ))
+            });
+            return news;
+        });
 }
-
+// getFirstArticle(apiUrl).then(console.log);
 
 (async () => {
   const response = await prompts({
     type: 'number',
-    name: 'news',
+    name: 'pageSize',
     message: 'How many news you'+'\''+'d like to see?'
   });
+  let newsList = [];
+  let algoliaPromise = getAlgolia(apiUrl).then( v => newsList.push(...v));
 
-  //console.log(response.news);
+  await algoliaPromise;
+  let pageNumber = 1; 
+  let startIndex = response.pageSize * (pageNumber -1);
+  console.log(newsList.slice(startIndex, startIndex + response.pageSize));
+
 })();
---
+++ src/model.js
@@ -0,0 +1,14 @@
+export class newsArticle {
+    title;
+    src;
+    url;
+    author;
+    date;
+    constructor(title, src, url, author, date) {
+      this.title = title;
+      this.src = src;
+      this.url = url;
+      this.author = author;
+      this.date = date;
+    }
+}
