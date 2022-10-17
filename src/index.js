f2264163ff14b14b8d83d11de8f6cb2a78c2e66d
David Mendes <david.mendes@mindswap.academy>
2022-10-17 09:47:29 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const prompts = require('prompts');
-//import {newsArticle} from './model.js'
 
 class newsArticle {
     title;
@@ -16,7 +15,7 @@ class newsArticle {
     }
 };
 
-// função getFirstArticle que retorna uma Promise para os detalhes da primeira notícia
+
 const apiUrl = "https://hn.algolia.com/api/v1/search";
 function getAlgolia(url){
     return fetch(url)
@@ -35,7 +34,6 @@ function getAlgolia(url){
             return news;
         });
 }
-// getFirstArticle(apiUrl).then(console.log);
 
 (async () => {
   const response = await prompts({
@@ -44,11 +42,9 @@ function getAlgolia(url){
     message: 'How many news you'+'\''+'d like to see?'
   });
   let newsList = [];
-  let algoliaPromise = getAlgolia(apiUrl).then( v => newsList.push(...v));
+  let algoliaPromise = getAlgolia(apiUrl).then( a => newsList.push(...a));
 
   await algoliaPromise;
-  let pageNumber = 1; 
-  let startIndex = response.pageSize * (pageNumber -1);
-  console.log(newsList.slice(startIndex, startIndex + response.pageSize));
+  console.log(newsList.slice(0, response.pageSize));
 
 })();
